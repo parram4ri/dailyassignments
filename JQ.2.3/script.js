b3dfae420a8b51e01f4e33dfb42d32c4d25adfc2
parram4ri <marianesabrinap@gmail.com>
2024-10-26 23:13:02 -0600

    completed JQ day 2, part 3

--- JQ.2.3/script.js
+++ JQ.2.3/script.js
@@ -1,34 +1,14 @@
-const storeItems = [
-    {
-      name: 'TV',
-      price: 800.00,
-      inStock: true
-    },
-    {
-      name: 'Phone',
-      price: 700.00,
-      inStock: false
-    },
-    {
-      name: 'Game Console',
-      price: 300.00,
-      inStock: true
-    },
-    {
-      name: 'Laptop',
-      price: 1200.00,
-      inStock: true
-    },
-    {
-      name: 'Smart Watch',
-      price: 200.00,
-      inStock: false
-    },
-    {
-      name: 'Headphones',
-      price: 100.00,
-      inStock: true
-    },
-  ];
+storeItems.forEach(item => {
+  let itemText = `${item.name}: $${item.price}`;
+  let itemClass = item.inStock ? "inStock" : "notInStock";
+ 
+  if (!item.inStock) {
+      itemText += " Not in stock";
+  }
+
+
+  $("#appendToMe").append(`<p class="${itemClass}">${itemText}</p>`);
+});
+
 
   
