608c6dd4e254360aa8d40cda4778313052d498ae
parram4ri <marianesabrinap@gmail.com>
2024-10-26 23:06:17 -0600

    completed JQ day 1, part 5

--- JQ.1.5/script.js
+++ JQ.1.5/script.js
@@ -1,4 +1,4 @@
-let parents = $("#selectMe").parents();
-let lastCard = $(parents).next();
-let p = $(lastCard).children()[1];
-$(p).text("Meant for large enterprises looking for an edge.");
+let parents = $("#selectMe").parents(".card");
+let lastCard = parents.next(".card");
+let p = lastCard.children("p");
+p.text("Meant for large enterprises looking for an edge.");
