283165c5995ed519d14e89336dc8342f10f9424d
parram4ri <marianesabrinap@gmail.com>
2024-10-26 23:00:32 -0600

    completed JQ day 1, part 3

--- JQ.1.3/script.js
+++ JQ.1.3/script.js
@@ -1,1 +1,4 @@
 let containerChildren = $('#mainCardsContainer').children();
+let containerChildren = $('#mainCardsContainer').children()[1];
+let h1 = $(containerChildren).children()[0];
+$(h1).text("Wonderful for small businesses. Scales easy! See if it's right for you!");
