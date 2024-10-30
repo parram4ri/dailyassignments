6149bd3af5ce7ac8cc9a7df29209451c29e92839
parram4ri <marianesabrinap@gmail.com>
2024-10-30 12:35:41 -0600

    completed JS day 1, part 2

--- JS.1.2/index.js
+++ JS.1.2/index.js
@@ -1,37 +1,21 @@
-// 1) Subtract startDistance from endDistance and assign the result to distanceTraveled
 
 let startDistance = 3;
 let endDistance = 22;
+let distanceTraveled = endDistance - startDistance;
+console.log(distanceTraveled);  
+let yardLength = 15;
+let yardWidth = 100;
+let yardArea = yardLength * yardWidth;
+console.log(yardArea);  
 
-let distanceTraveled;
+let firstHeartRate = 60;
+let secondHeartRate = 63;
+let thirdHeartRate = 59;
+let averageHeartRate = (firstHeartRate + secondHeartRate + thirdHeartRate) / 3;
+console.log(averageHeartRate);  
 
-// 2) We need to figure out the area of my backyard to know exactly how much yard I have.
-// The length of my yard is 15ft and the width of my yard is 100ft.
-// Create variables for each and assign the result to a yardArea variable.
-// Hint: Area is length times width.
+let result = 2 + (3 * 4);
+console.log(result);
 
-let yardWidth;
-let yardLength;
-
-let yardArea;
-
-// 3) I would like to figure out my resting heart rate.
-// To be accurate I should take my resting heart rate multiple times and find the average
-// to get a more accurate result. The first heart rate was 60BPM
-// the second was 63BPM and the third was 59BPM.
-// Assign each of these numbers to variables and assign the average heart rate
-// using each of these variables to the averageHeartRate variable.
-
-let firstHeartRate;
-let secondHeartRate;
-let thirdHeartRate;
-
-let averageHeartRate;
-
-// 4) We have an order of operations error here. Add parenthesis so the result is 20.
-
-let result = 2 + 3 * 4;
-
-// 5) Another order of operations error. The result should be 60.
-
-let result2 = 3 + 7 * -4 + 10;
+let result2 = (3 + 7) * 4 + 10;
+console.log(result2); 
