77bbeeddeff337323efaa5094cc8911e60d1c83a
parram4ri <marianesabrinap@gmail.com>
2024-10-30 12:50:28 -0600

    completed JS day 1, part 3

--- JS.1.3/index.js
+++ JS.1.3/index.js
@@ -1,26 +1,11 @@
-// 1) I need a fence around my yard now. Help me find the perimeter.
-// Perimeter is width + with + length + length
 
 let yardWidth = 100;
 let yardLength = 15;
+let yardPerimeter = (yardWidth * 2) + (yardLength * 2);
+console.log(yardPerimeter);  
 
-let yardPerimeter;
-
-
-// 2) With the yardPerimeter variable go ahead and use back ticks (``)
-// with string interpolation (${}) to put the following message
-// in the messagePerimeter variable -->
-// 'You are going to need (insert perimeter here)ft of fence'.
-// Don't remember how to do this? Go back to the first repl.it assignment from today
-// and look at the example problems console.log().
-
-let messagePerimeter;
-
-// 3)One last thing. It is going to cost me $15 per foot of fence.
-// What is the total price I am going to have to pay?
-// Use the yardPerimeter variable and the pricePerFoot variable
-// to find the total price and put that value in the totalPrice variable.
-
+let messagePerimeter = `You are going to need ${yardPerimeter}ft of fence.`;
+console.log(messagePerimeter); 
 let pricePerFoot = 15;
-
-let totalPrice;
+let totalPrice = yardPerimeter * pricePerFoot;
+console.log(totalPrice); 
