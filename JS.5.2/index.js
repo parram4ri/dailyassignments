800cb17c542e9d6e29bab07c80e22b948472ea89
parram4ri <marianesabrinap@gmail.com>
2024-10-31 15:07:08 -0600

    Completed JS day 5, part 2

--- JS.5.2/index.js
+++ JS.5.2/index.js
@@ -1,58 +1,53 @@
-// MAKE SURE you use the array functions to complete these problems.
-
 // Problem 1
-// Below we have an array. Take the last item off of the array.
+// Take the last item off of the array using .pop()
 
 let array1 = [1, 2, 3, 4, 5, 6];
 function removeLastItem() {
-  //write code here
+  array1.pop();
 }
 
 
 // Problem 2
-// Below we have an array. Take the first item off of the array.
+// Take the first item off of the array using .shift()
 
 let array2 = ["This", "Hello", "World"];
 function removeFirstItem() {
-  //write code here
+  array2.shift(); 
 }
 
 
 // Problem 3
-// Below we have an array. Remove "stuff" and "ready" from the array.
+// Remove "stuff" and "ready" from the array using .splice()
 
 let array3 = ["well", "stuff", "money", "ready", "hello"];
 function removeItems() {
-  //write code here
-  array3.splice(1, 1);
-  array3.splice(2, 1);
+  array3.splice(1, 1); 
+  array3.splice(2, 1); 
 }
 
 
-
 // Problem 4
-// Below we have an array. Add "well" to the beginning of the array.
+// Add "well" to the beginning of the array using .unshift()
 
 let array4 = ["hello", "people"];
-
 function addItem() {
-  //write code here
+  array4.unshift("well"); 
 }
 
 
 // Problem 5
-// Below we have an array. Add "power" in between "great" and "comes".
+// Add "power" between "great" and "comes" using .splice()
 
 let array5 = ["with", "great", "comes", "great", "responsibility"];
 function addMoreItem() {
-  //write code here
+  array5.splice(2, 0, "power"); 
 }
 
+
 // Problem 6
-// Below we have an array. Add 7 to the end of the array.
+// Add 7 to the end of the array using .push()
 
 let array6 = [1, 2, 3, 4, 5, 6];
 function addLast() {
-  //write code here
+  array6.push(7);
 }
-
