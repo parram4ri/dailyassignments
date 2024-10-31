381b2506061fa9066127d381a0ba885716838a71
parram4ri <marianesabrinap@gmail.com>
2024-10-31 14:34:42 -0600

    completed JS day 3, part 2

--- JS.3.2/index.js
+++ JS.3.2/index.js
@@ -1,11 +1,7 @@
-// Go to instructions.md file first!
-
-// Problem
-// Demonstrate a do while loop
-// In the loop increment i by one.
-// Have the condition always result to false so the loop will
-// only loop once.
-
 let i = 5;
 
-//write loop here.
+do {
+  i++;
+} while (false); 
+
+console.log(i); 
