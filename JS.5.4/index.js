5297d9f6293c5054042189f9c25c5daae9156d15
parram4ri <marianesabrinap@gmail.com>
2024-10-31 15:16:35 -0600

    Completed JS day 5, part 4

--- JS.5.4/index.js
+++ JS.5.4/index.js
@@ -1,11 +1,6 @@
 // Problem 1
-// Below we have an array of objects.
-// Loop through the array of objects and add up all the prices if the
-// item is in stock.
+// Loop through the array of objects and add up all the prices if the item is in stock.
 // Must use forEach on shoppingCart
-// Have the totalPrice function return the total price
-// DO NOT modify the original shoppingCart array. The tests will not pass If
-// you do so.
 
 let shoppingCart = [
     {
@@ -31,22 +26,20 @@ let shoppingCart = [
   ];
 
 function totalPrice() {
-  //write code here
+  let total = 0;
   
+  shoppingCart.forEach(item => {
+    if (item.inStock) {
+      total += item.price;
+    }
+  });
   
+  return total; 
 }
 
 
 // Problem 2
-// We have an array of items in a store.
-// The manager needs to know which items are in stock.
-// Use forEach to loop through our array of objects.
-// Create a new array with ONLY the name of each item
-// that is in stock. Hint: You are going to need to 
-// create a new array and PUSH each item name to the 
-// end of the array.
-// Do not modify the original shopItems array.
-// The tests will not pass if you do so.
+// Create a new array with ONLY the name of each item that is in stock.
 
 const shopItems = [
     {
@@ -87,8 +80,13 @@ const shopItems = [
   ];
 
 function inStockItems() {
-  //write code here
+  let inStockNames = [];
   
-  //return the array of in stock items
+  shopItems.forEach(item => {
+    if (item.inStock) {
+      inStockNames.push(item.itemName);
+    }
+  });
   
+  return inStockNames; 
 }
