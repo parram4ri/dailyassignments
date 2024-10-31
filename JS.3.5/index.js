45465cfd8e7bad5f4793229c6b10a73b90c51734
parram4ri <marianesabrinap@gmail.com>
2024-10-31 14:52:03 -0600

    Completed JS day 3, part 5

--- JS.3.5/index.js
+++ JS.3.5/index.js
@@ -1,1 +1,31 @@
-//Go to the instructions.md file first thing!
+function rockPaperScissors(userChoice) {
+  userChoice = userChoice.toLowerCase();
+
+  const randomNum = Math.floor(Math.random() * 3);
+
+  let computerChoice;
+  if (randomNum === 0) {
+    computerChoice = "rock";
+  } else if (randomNum === 1) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+
+  if (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors") {
+    return "I do not understand. Try entering rock, paper, or scissors.";
+  }
+
+  if (userChoice === computerChoice) {
+    return `It's a tie! I chose ${computerChoice}.`;
+  } else if (
+    (userChoice === "rock" && computerChoice === "scissors") ||
+    (userChoice === "scissors" && computerChoice === "paper") ||
+    (userChoice === "paper" && computerChoice === "rock")
+  ) {
+    return `You win! I chose ${computerChoice}.`;
+  } else {
+    return `You lose! I chose ${computerChoice}.`;
+  }
+}
+
