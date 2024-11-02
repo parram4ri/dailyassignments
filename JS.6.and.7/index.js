9b41f6f77d42e646e974814048320056325bf18d
parram4ri <marianesabrinap@gmail.com>
2024-11-02 14:33:48 -0600

    completes JS 6 and 7

--- JS.6.and.7/index.js
+++ JS.6.and.7/index.js
@@ -1,13 +1,57 @@
+// Random word selection
 function randomWords() {
     let words = ['stuff', 'sugar', 'sweet', 'happy', 'money', 'cat', 'make'];
     let index = Math.floor(Math.random() * words.length);
     return words[index];
 }
-
-//Random word selected
 let theWord = randomWords();
 
-//click run a few times to see this in action then delete this line of code.
-console.log(theWord);
+let wordDisplay = Array(theWord.length).fill('_').join(' '); 
+let wrongGuesses = [];
+let maxTries = 6; 
+let remainingTries = maxTries;
+let guessedLetters = [];
+let gameWon = false;
 
-//Write your code here.
+while (remainingTries > 0 && !gameWon) {
+    let userDisplay = `Gameboard: ${wordDisplay}\nTries left: ${remainingTries}\nWrong letters guessed: ${wrongGuesses.join(', ')}\n`;
+    let userGuess = prompt(userDisplay + "Guess a letter or the whole word:");
+
+    if (userGuess === theWord) {
+        alert("Congratulations! You've guessed the word correctly.");
+        gameWon = true;
+        break;
+    }
+
+    if (userGuess.length === 1) {
+        if (guessedLetters.includes(userGuess) || wrongGuesses.includes(userGuess)) {
+            alert("You've already guessed that letter. Try again.");
+            continue;
+        }
+        
+        guessedLetters.push(userGuess);
+
+        if (theWord.includes(userGuess)) {
+            let updatedDisplay = "";
+            for (let i = 0; i < theWord.length; i++) {
+                updatedDisplay += (theWord[i] === userGuess || guessedLetters.includes(theWord[i])) ? theWord[i] : '_';
+            }
+            wordDisplay = updatedDisplay.split('').join(' ');
+
+            if (!wordDisplay.includes('_')) {
+                alert("Congratulations! You've won the game.");
+                gameWon = true;
+                break;
+            }
+        } else {
+            wrongGuesses.push(userGuess);
+            remainingTries--;
+        }
+    } else {
+        alert("Please guess a single letter or the entire word.");
+    }
+}
+
+if (!gameWon) {
+    alert(`Game over! You've run out of tries. The correct word was: "${theWord}".`);
+}
